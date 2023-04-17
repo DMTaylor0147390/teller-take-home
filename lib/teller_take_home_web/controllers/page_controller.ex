a8dc01c113448f6b7d9acbb1af575010246fc0b4
Derek M Taylor <derekmtaylor@MacBook-Pro-2.local>
2023-04-17 00:31:12 -0400

    Better error handling + Postman docs export

--- lib/teller_take_home_web/controllers/page_controller.ex
+++ lib/teller_take_home_web/controllers/page_controller.ex
@@ -6,21 +6,32 @@ defmodule TellerTakeHomeWeb.PageController do
   end
 
   def list_accounts(conn, _params) do
-    {status_code, response} = conn.req_headers
-    |> TellerTakeHome.Requests.list_accounts()
+    {status_code, response} = try do
+                                conn.req_headers
+                                |> TellerTakeHome.Requests.list_accounts()
+                              rescue
+                                e -> {500, inspect(e)}
+                              end
 
     send_resp(conn, status_code, response |> Jason.encode!)
   end
 
   def enroll(conn, params) do
-    {status_code, response} = TellerTakeHome.Requests.enroll(params, conn.req_headers)
+    {status_code, response} = try do
+                                TellerTakeHome.Requests.enroll(params, conn.req_headers)
+                              rescue
+                                e -> {500, inspect(e)}
+                              end
     send_resp(conn, status_code, response)
   end
 
   def list_transactions(conn, params) do
-    {status_code, response} = params["account"]
-    |> TellerTakeHome.Requests.list_transactions(conn.req_headers)
-
+    {status_code, response} = try do
+                                params["account"]
+                                |> TellerTakeHome.Requests.list_transactions(conn.req_headers)
+                              rescue
+                                e -> {500, inspect(e)}
+                              end
     send_resp(conn, status_code, Jason.encode!(response))
   end
 end
